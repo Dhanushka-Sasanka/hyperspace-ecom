07fbdf47dd79570722ce56d83b1cb041710b376b
Dhanushka_Sasanka <dhanushkasasankapayza@gmal.com>
2021-08-08 19:08:26 +0530

    implemented some UI

--- hyperspace-advertisement-frontend/src/app/app-routing.module.ts
+++ hyperspace-advertisement-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+    {
+      path: 'home',
+      component: LandingPageComponent,
+    },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hyperspace-advertisement-frontend/src/app/app.module.ts
+++ hyperspace-advertisement-frontend/src/app/app.module.ts
@@ -1,12 +1,29 @@
+import { LoginComponent } from './pages/login/login.component';
+import { UserComponent } from './pages/user/user.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegisterComponent } from './pages/register/register.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MainNavBarComponent } from './pages/landing-page/main-nav-bar/main-nav-bar.component';
+import { CategoryComponent } from './pages/landing-page/category/category.component';
+import { MainSliderComponent } from './pages/landing-page/main-slider/main-slider.component';
+import { FooterComponent } from './pages/landing-page/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent,
+    MainNavBarComponent,
+    CategoryComponent,
+    MainSliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
